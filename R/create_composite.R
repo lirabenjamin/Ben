7832bff62bb6d53a84ad0f5e150dfbd2cdffbfa2
lirabenjamin <lira.benjamin@gmail.com>
2022-06-27 14:34:14 -0400

    Create create_composite.R

---
+++ R/create_composite.R
@@ -0,0 +1,32 @@
+#' Create Composites
+#'
+#' This function will take as input a dataset, a selection of columns, and a name for the column to be created
+#'
+#' @param data
+#' The data to add a composite column to
+#'
+#' @param selection
+#' A tidy selection of the variables that are to be averaged together
+#'
+#' @param name
+#' The name of the variable to be created
+#'
+#' @param na.rm
+#' Set to TRUE (default), will adjust the denominator in the case of missing data. If set to false, will return NA if the row has any missing data
+#'
+#' @return
+#' a tibble object
+#'
+#' @export
+#'
+#' @examples
+
+create_composite <-
+  function(data,selection, name, na.rm = T) {
+    mtcars |>
+      dplyr::rowwise() |>
+      dplyr::mutate({{name}} := mean(c({{selection}}, na.rm = T))) |>
+      dplyr::ungroup()
+  }
+
+mtcars |> create_composite(c(drat, gear), drat_gear_average)
