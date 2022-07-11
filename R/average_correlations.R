f2325599f1c043a1af843e6f7cae64620ab9ea52
lirabenjamin <lira.benjamin@gmail.com>
2022-07-11 12:14:22 -0400

    Added weights according to paper in the help section

--- R/average_correlations.R
+++ R/average_correlations.R
@@ -2,6 +2,8 @@
 #'
 #' This function will take as input a verctor of correlations and output their mean after being transformed to fisher's Zs and back
 #'
+#'see https://www.tandfonline.com/doi/pdf/10.1080/00221309809595548?needAccess=true
+#'
 #' @param correlations
 #' The vector of correlations to average
 #'
@@ -11,11 +13,20 @@
 #' @export
 #'
 #' @examples
-#' mtcars |> cor()
+#' c(.40, .04, .23, .25, .94, .01) |> average_correlations()
+#' c(.40, .04, .23, .25, .94, .01) |> average_correlations(ns = c(10, 20, 40, 23, 10, 123))
+#' c(.40, .04, .23, .25, .94, .01) |> average_correlations(ns = c(10, 20, 40, 23, 10, 123), use_weights = F)
+#' c(.40, .04, .23, .25, .94, .01) |> average_correlations(ns = c(10, 20, 40, 23, 10, 123), use_weights = T)
+
 
 average_correlations <-
-  function(correlations, na.rm = T) {
-    correlations |> psych::fisherz() |> mean(na.rm = na.rm) |> psych::fisherz2r()
+  function(correlations,ns, na.rm = T, use_weights = F) {
+    if(use_weights){
+      r = correlations |> psych::fisherz() |> weighted.mean(w = ns, na.rm = na.rm) |> psych::fisherz2r()
+      return(r)
+    }
+    if(!use_weights){
+      r = correlations |> psych::fisherz() |> mean(na.rm = na.rm) |> psych::fisherz2r()
+      return(r)
+    }
   }
-
-
